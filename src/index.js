57c9b8b9e905122e5903d1889b4cb4084b0ffbb5
hulki <muhamadamin1992@gmail.com>
2019-07-06 18:09:28 +0500

    Implement Brackets with recursion and loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  'use strict';
+
+  const OPENS = bracketsConfig.map( value => value[0] ),
+  			CLOSES = bracketsConfig.map( value => value[1] ),
+  			LENGTH = str.length;
+
+  let pos = 0;
+
+  let checkBrackets = ( pos ) => {
+
+  	let index = OPENS.indexOf( str[pos] );
+
+  	if ( index === -1 ) {
+  		return false;
+  	}
+
+  	if ( str[pos + 1] === CLOSES[index] ) {
+  		return pos + 2;
+  	}
+
+	  pos = checkBrackets( pos + 1 );
+
+  	while ( pos < LENGTH && pos !== false ) {
+
+	  	if ( str[pos] === CLOSES[index] ) {
+	  		return pos + 1;
+	  	}
+
+			pos = checkBrackets( pos );
+  		
+  	}
+
+  	if ( pos === false ) {
+  		return false;
+  	}
+
+  	if ( pos === LENGTH ) {
+  		return false;
+  	}
+
+  	return pos;
+  };
+
+  while ( pos < LENGTH && pos !== false ) {
+  	pos = checkBrackets( pos )
+  }
+
+  return !!pos;
 }
